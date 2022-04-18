e52c8b47c9b9da1f617e1a1c52f20991fd16caa1
Ankit Kumar <ankitkumar@Ankits-MacBook-Air.local>
2022-04-18 16:43:49 +0530

    fetching data done

---
+++ server/src/routes/data.js
@@ -0,0 +1,55 @@
+var express = require('express');
+
+
+const { BigQuery } = require('@google-cloud/bigquery');
+// Defines the location of the dataset and tables
+const location = 'US';
+
+
+const bigquery = new BigQuery({
+  // The relative file path to your Service Account key file
+  keyFilename: '/Users/ankitkumar/Documents/bigquery/key.json',
+  // The GCP project ID we want to work in
+  projectId: 'demand-ml-solution'
+});
+
+
+
+const getData = async (projectId, datasetId, tableId, mid) => {
+  const query = `
+        SELECT * from \`${projectId}.${datasetId}.${tableId}\`
+        where MasterMailingID = ${mid}
+    `;
+  const options = {
+    query: query,
+    // Location must match that of the dataset(s) referenced in the query.
+    location,
+  };
+
+  // Run the query as a job
+  const [job] = await bigquery.createQueryJob(options);
+  console.log(`Job '${job.id}' started.\n`);
+
+  // Wait for the query to finish
+  const [rows] = await job.getQueryResults();
+
+
+  // Print the results
+  console.log('Resulted Rows:');
+  rows.forEach(row => console.log(row));
+}
+
+
+var app = express();
+
+app.get('/fetch/:id', function (req, res) {
+  var id = req.params.id;
+  var projectId = "demand-ml-solution";
+  var datasetId = "Rawdata";
+  var tableId = "CampaignRegistry";
+  getData(projectId, datasetId, tableId, id);
+  console.log("this is working");
+})
+
+
+module.exports = app;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,11 +1,14 @@
 var express = require("express");
 var router = express.Router();
 const path = require("path");
+var data = require("./data");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   let indexFile = path.join(__dirname, "../../static/index.html");
   res.sendFile(indexFile);
 });
+
+router.use('/data', data);
 
 module.exports = router;
